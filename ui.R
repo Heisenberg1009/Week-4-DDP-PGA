b36e9587b0278d0e66d7c93c01d94bffd09a88ca
Matty23 <84488856+Heisenberg1009@users.noreply.github.com>
2022-11-03 08:39:57 +0530

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,77 @@
+library(shiny)
+
+shinyUI(
+  navbarPage("Shiny Application",
+             tabPanel("Analysis",
+                      fluidPage(
+                        titlePanel("The relationship between variables and miles per gallon (MPG)"),
+                        sidebarLayout(
+                          sidebarPanel(
+                            selectInput("variable", "Variable:",
+                                        c("Number of cylinders" = "cyl",
+                                          "Displacement (cu.in.)" = "disp",
+                                          "Gross horsepower" = "hp",
+                                          "Rear axle ratio" = "drat",
+                                          "Weight (lb/1000)" = "wt",
+                                          "1/4 mile time" = "qsec",
+                                          "V/S" = "vs",
+                                          "Transmission" = "am",
+                                          "Number of forward gears" = "gear",
+                                          "Number of carburetors" = "carb"
+                                        )),
+                            
+                            checkboxInput("outliers", "Show BoxPlot's outliers", FALSE)
+                          ),
+                          
+                          mainPanel(
+                            h3(textOutput("caption")),
+                            
+                            tabsetPanel(type = "tabs", 
+                                        tabPanel("BoxPlot", plotOutput("mpgBoxPlot")),
+                                        tabPanel("Regression model", 
+                                                 plotOutput("mpgPlot"),
+                                                 verbatimTextOutput("fit")
+                                        )
+                            )
+                          )
+                        )
+                      )
+             ),
+             tabPanel("About the Data Set",
+                      
+                      h3("Regression Models Course Project (from Coursera)"),
+                      helpText(" Scenario: You work for Motor Trend, a magazine about the automobile industry Looking at a data set of a collection of cars, they are interested in exploring the relationship",
+                               "between a set of variables and miles per gallon (MPG) (outcome). They are particularly interested in the following two questions: Is an automatic or manual transmission better for MPG. Quantify the MPG difference between automatic and manual transmissions"),
+                      h3("Important"),
+                      p("A data frame with 32 observations on 11 features."),
+                      
+                      a("https://class.coursera.org/regmods-008")
+             ),
+             tabPanel("Data Detail",
+                      h2("Motor Trend Car Road Tests"),
+                      hr(),
+                      h3("Description"),
+                      helpText("The data was extracted from '74 Motor Trend US magazine,",
+                               " which contains details about fuel consumption and other aspects of automobile performance and design parameters",
+                               " for 32 automobiles (1973-74 models)."),
+                      h3("Shape of the dataset"),
+                      p("Dataset consists of 32 observations on 11 features which are as follows:"),
+                      
+                      p("  [, 1]   mpg         Miles/(US) gallon"),
+                      p("  [, 2]	 cyl	 Number of cylinders"),
+                      p("  [, 3]	 disp	 Displacement (cu.in.)"),
+                      p("  [, 4]	 hp	 Gross horsepower"),
+                      p("  [, 5]	 drat	 Rear axle ratio"),
+                      p("  [, 6]	 wt	 Weight (lb/1000)"),
+                      p("  [, 7]	 qsec	 1/4 mile time"),
+                      p("  [, 8]	 vs	 V/S"),
+                      p("  [, 9]	 am	 Transmission (0 = automatic, 1 = manual)"),
+                      p("  [,10]	 gear	 Number of forward gears"),
+                      p("  [,11]	 carb	 Number of carburetors"),
+                      
+                      
+             ),
+             
+             )
+  
+)
